87f30e50eaec0bddb4ea693ae6ed637bfdc8fa40
Harsh Mishra <hmishra927@gmail.com>
2023-10-05 21:08:06 +0530

    uploading to github

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-7323041554 </p>
+         <p> <i class="fas fa-phone"></i> +91-9931053869 </p>
+         <p> <i class="fas fa-envelope"></i> chandankumar2021022@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <!-- <a href="#"> <i class="fab fa-twitter"></i> twitter </a> -->
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/harsh-mishra-968269221"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Chandan|Ajit|Harsh</span> </p>
+
+</section>
